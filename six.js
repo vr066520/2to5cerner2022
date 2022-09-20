bfe4645ed9f1e5b6adc5b67d5b91233d709bdd36
vr066520 <vidyashree.r@cerner.com>
2022-09-20 18:20:08 +0530

    six 2to 5
    six 2 to 5

---
+++ six.js
@@ -0,0 +1,32 @@
+function thirdLargest(arr, arr_size)
+{//cerner_2^5_2022
+    if (arr_size < 3)
+    {
+        document.write(" Invalid Input ");
+        return;
+    }   
+    let first = arr[0];
+    for (let i = 1;
+             i < arr_size ; i++)
+        if (arr[i] > first)
+            first = arr[i];     
+    let second = Number.MIN_VALUE;
+    for (let i = 0;
+             i < arr_size ; i++)
+        if (arr[i] > second &&
+            arr[i] < first)
+            second = arr[i]; 
+    let third = Number.MIN_VALUE;
+    for (let i = 0;
+             i < arr_size ; i++)
+        if (arr[i] > third &&
+            arr[i] < second)
+            third = arr[i];   
+    document.write("The third Largest " +
+                  "element is ", third);
+} 
+    let arr = [12, 13, 1,
+                 10, 34, 16];
+    let n = arr.length;
+    thirdLargest(arr, n);                         
+</script>
