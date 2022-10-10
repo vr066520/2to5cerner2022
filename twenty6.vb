a919f46f5e6c72e3e767169a00512a61041f9ce5
vr066520 <vidyashree.r@cerner.com>
2022-10-10 22:11:54 +0530

    twenty6 2 to 5
    twenty6 2 to 5

---
+++ twenty6.vb
@@ -0,0 +1,23 @@
+Module Module1
+    Sub Main()
+'cerner_2^5_2022
+       Dim students(0 to 2) As String 
+
+        students(0) = "John"
+
+        students(1) = "Alice"
+
+        students(2) = "Antony"
+
+        Console.WriteLine("First student is {0} ", students(0))
+
+        Console.WriteLine("Second student is {0} ", students(1))
+
+        Console.WriteLine("Third student is {0} ", students(2))
+
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
